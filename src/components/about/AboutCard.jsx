9c6290baa3b0d65e130c79ef2625721e886cd5ab
sadik alike <sadikalike7@gmail.com>
2024-10-18 20:38:12 +0530

    Third commit4

--- src/components/about/AboutCard.jsx
+++ src/components/about/AboutCard.jsx
@@ -1,23 +1,34 @@
-import React from "react"
-import Heading from "../common/heading/Heading"
-import "./about.css"
-import { homeAbout } from "../../dummydata"
-import Awrapper from "./Awrapper"
+import React from "react";
+import Heading from "../common/heading/Heading";
+import "./about.css";
+import { homeAbout } from "../../dummydata";
+import { useInView } from 'react-intersection-observer';
 
 const AboutCard = () => {
+  // Using Intersection Observer hook
+  const { ref, inView } = useInView({
+    triggerOnce: false, // Animation triggers each time it enters view
+    threshold: 0.2, // Element should be 20% visible to trigger
+  });
+
   return (
     <>
       <section className='aboutHome'>
         <div className='container flexSB'>
+          {/* Left side with image */}
           <div className='left row'>
             <img src='./images/about.webp' alt='' />
           </div>
+
+          {/* Right side with content */}
           <div className='right row'>
             <Heading subtitle='LEARN ANYTHING' title='Benefits About Online Learning Expertise' />
-            <div className='items'>
+
+            {/* Applying the scroll animation on the container */}
+            <div className={`items ${inView ? 'visible' : ''}`} ref={ref}>
               {homeAbout.map((val) => {
                 return (
-                  <div className='item flexSB'>
+                  <div className='item flexSB' key={val.id}>
                     <div className='img'>
                       <img src={val.cover} alt='' />
                     </div>
@@ -26,15 +37,14 @@ const AboutCard = () => {
                       <p>{val.desc}</p>
                     </div>
                   </div>
-                )
+                );
               })}
             </div>
           </div>
         </div>
       </section>
-      {/* <Awrapper /> */}
     </>
-  )
-}
+  );
+};
 
-export default AboutCard
+export default AboutCard;
