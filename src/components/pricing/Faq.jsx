fc6ab61876d6bf09c011dcbd1c280be5d94a424f
sadik alike <sadikalike7@gmail.com>
2025-06-20 15:18:34 +0530

    updated 82

--- src/components/pricing/Faq.jsx
+++ src/components/pricing/Faq.jsx
@@ -44,8 +44,8 @@ const Faq = () => {
 
             <div className='items grid2'>
               <div className='box'>
-                <h4>ADDRESS:</h4>
-                <p>Mangaluru, Dakshina Kannada , Karnataka</p>
+                {/* <h4>ADDRESS:</h4>
+                <p>Mangaluru, Dakshina Kannada , Karnataka</p> */}
               
               {/* <div className='box'>
                 <h4>EMAIL:</h4>
