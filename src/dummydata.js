abd5536e9225cdff4c5f3b8cd6e3723dabb0f130
sadik alike <sadikalike7@gmail.com>
2026-02-07 09:28:25 +0530

    Added navbar and footer sectio1n1ab

--- src/dummydata.js
+++ src/dummydata.js
@@ -398,7 +398,7 @@ export const testimonal = [
     name: "ROGER SCOTT",
     post: "MARKETING MANAGER",
     desc: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
-    // cover: "./images/testo/t1.webp",
+    cover: "./images/testo/t1.webp",
   },
   {
     id: 2,
